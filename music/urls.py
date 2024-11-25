e835ba34db7c30c243b16032da01636a5674bee4
Anonna <meftahul@iut-dhaka.edu>
2024-11-25 21:41:20 +0600

    updates

--- music/urls.py
+++ music/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('', views.index, name='index'),
     path('signup/', views.signup_view, name='signup'),
     path('signup2/', views.signup_view, name='signup2'),
     path('login/', views.login_view, name='login'),
     path('login2/', views.login_view, name='login2'),
     # path('play/', views.song_list, name='song_list'),
     path('', views.song_list, name='song_list'),
+    path('', views.index, name='index'),
     path('song/<int:song_index>/', views.song_detail, name='song_detail'),
     path("play_song/", views.play_song, name="play_song"),
     path('search/', views.search, name='search'),
